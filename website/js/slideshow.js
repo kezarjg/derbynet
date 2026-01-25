7c0c347e9e59313bcc95338ec7ec3c8f7b64b7fd
Jerry Kezar <jerry@kezarnet.com>
2026-01-25 16:20:08 +0000

    Add keyboard controls and award indicators to slideshow
    - Spacebar toggles pause/play
    - Arrow keys for manual slide navigation with proper history
    - Display award indicators on racer slides (speed, regular, ad-hoc)
    - Regular awards show the Den/class name
    - Press ? for help overlay showing keyboard shortcuts
    - Auto-resume after 30 seconds of inactivity
    - Support direct link to racer via car number: slideshow.php?car=123

--- website/js/slideshow.js
+++ website/js/slideshow.js
@@ -19,13 +19,18 @@ function mainphoto_onload(img) {
 
 
 (function() {
-  var current_racer_id = 0;
-  // This cache breaker value is for the title slide image.  We want that image
-  // to be cached for the duration of the slideshow.  Reloading the slideshow
-  // will pick up any changes to the title slide image.
-  var cachebreaker = Date.now();
   // g_kiosk_parameters is set in slideshow.php
   var kiosk_parameters = g_kiosk_parameters;
+
+  // Slideshow state
+  var is_paused = false;
+  var slide_interval;
+  var manual_advance_timeout;
+  var pending_ajax = null;
+
+  // Navigation history: array of slides with an index pointer
+  var slide_history = [];
+  var history_index = -1;  // Current position in history
 
   try {
     KioskPoller.param_callback = function(parameters) {
@@ -37,27 +42,33 @@ function mainphoto_onload(img) {
     // If not in a kiosk, then KioskPoller will be undefined.
   }
 
-  var next_query = {'mode': 'slide',
-                    'file': ''};
-
-  // response: {'photo', 'inset', 'name', 'carname', 'carnumber', 'next'}
-  function refresh_page(response) {
-    next_query = response.next;
-
-    // There's always a div.next, which is hidden; we populate it with images
-    // for the next racer.  When the next racer becomes current, the old
-    // div.current gets removed, div.next becomes div.current, and we create a
-    // new div.next.
+  // Display a slide on screen
+  function display_slide(response) {
+    // Remove old current slide and any pending next slide
     $("#photo-background div.current").remove();
-    $("#photo-background div.next").removeClass("next").addClass("current");
+    $("#photo-background div.next").remove();
 
-    var next = $("<div class='next'></div>").appendTo("#photo-background")
+    // Create new slide as current (visible immediately)
+    var current = $("<div class='current'></div>").appendTo("#photo-background")
         .append($("<img class='mainphoto'/>")
                 .attr('src', response['photo'])
                 .on('load', function() { mainphoto_onload(this); }));
 
+    // Add award indicators if awards exist
+    if (response.hasOwnProperty('awards') && response['awards'].length > 0) {
+      var award_container = $("<div class='award_indicators'></div>").appendTo(current);
+      for (var i = 0; i < response['awards'].length; i++) {
+        var award = response['awards'][i];
+        var award_class = award.type + '_award';
+
+        $("<div class='award_indicator " + award_class + "'></div>")
+          .text(award.name)
+          .appendTo(award_container);
+      }
+    }
+
     if (response.hasOwnProperty('name')) {
-      var subtitle = $("<p class='subtitle'/>").text(response['name']).appendTo(next);
+      var subtitle = $("<p class='subtitle'/>").text(response['name']).appendTo(current);
       if (response.hasOwnProperty('carnumber')) {
         subtitle.prepend(': ')
                 .prepend($("<span class='carno'/>").text(response['carnumber']));
@@ -69,15 +80,30 @@ function mainphoto_onload(img) {
     }
 
     if (response.hasOwnProperty('inset')) {
-      next.append('<img class="inset_photo" src="' + response['inset'] + '"/>');
+      current.append('<img class="inset_photo" src="' + response['inset'] + '"/>');
     }
 
     if (response.hasOwnProperty('title') && kiosk_parameters.title) {
-      $('<p class="maintitle"></p>').text(kiosk_parameters.title).appendTo(next);
+      $('<p class="maintitle"></p>').text(kiosk_parameters.title).appendTo(current);
     }
   }
 
-  function slide_poll() {
+  // Add a new slide to history and display it
+  function add_and_display(response) {
+    // If we navigated back and then got a new slide, truncate forward history
+    if (history_index < slide_history.length - 1) {
+      slide_history = slide_history.slice(0, history_index + 1);
+    }
+    slide_history.push(response);
+    history_index = slide_history.length - 1;
+    display_slide(response);
+  }
+
+  // Fetch the next slide from server
+  function fetch_next_slide() {
+    var current = slide_history[history_index];
+    var next_query = current ? current.next : {'mode': 'slide', 'file': ''};
+
     next_query.query = 'slide.next';
     if (kiosk_parameters.subdir) {
       next_query.subdir = kiosk_parameters.subdir;
@@ -86,15 +112,15 @@ function mainphoto_onload(img) {
     if (classids && classids.length > 0) {
       next_query.classids = classids.join(',');
     }
-    $.ajax('action.php',
+    pending_ajax = $.ajax('action.php',
            {type: 'GET',
             data: next_query,
             success: function(data) {
-              console.log('slideshow next', data);
+              pending_ajax = null;
               if (data.hasOwnProperty('photo')) {
-                refresh_page(data.photo);
+                add_and_display(data.photo);
               } else {
-                refresh_page({'photo': 'slide.php/title',
+                add_and_display({'photo': 'slide.php/title',
                               'title': true,
                               'next': {'mode': 'slide',
                                        'file': ''}});
@@ -104,21 +130,134 @@ function mainphoto_onload(img) {
           );
   }
 
+  function start_slideshow() {
+    if (slide_interval) {
+      clearInterval(slide_interval);
+    }
+    slide_interval = setInterval(function() {
+      // Always fetch next when auto-advancing
+      fetch_next_slide();
+    }, 10000);
+    is_paused = false;
+    $("#pause-indicator").removeClass("visible");
+  }
+
+  function pause_slideshow() {
+    if (slide_interval) {
+      clearInterval(slide_interval);
+      slide_interval = null;
+    }
+    is_paused = true;
+    if (manual_advance_timeout) {
+      clearTimeout(manual_advance_timeout);
+      manual_advance_timeout = null;
+    }
+    if (pending_ajax) {
+      pending_ajax.abort();
+      pending_ajax = null;
+    }
+  }
+
+  function toggle_pause_play() {
+    if (is_paused) {
+      start_slideshow();
+    } else {
+      pause_slideshow();
+      $("#pause-indicator").addClass("visible");
+    }
+  }
+
+  function manual_next_slide() {
+    pause_slideshow();
+    $("#pause-indicator").addClass("visible");
+
+    // If we have forward history, use it; otherwise fetch new
+    if (history_index < slide_history.length - 1) {
+      history_index++;
+      display_slide(slide_history[history_index]);
+    } else {
+      fetch_next_slide();
+    }
+
+    manual_advance_timeout = setTimeout(start_slideshow, 30000);
+  }
+
+  function manual_prev_slide() {
+    pause_slideshow();
+    $("#pause-indicator").addClass("visible");
+
+    if (history_index > 0) {
+      history_index--;
+      display_slide(slide_history[history_index]);
+    }
+
+    manual_advance_timeout = setTimeout(start_slideshow, 30000);
+  }
+
   $(document).ready(function() {
+    // Keyboard event handler
+    $(document).keydown(function(e) {
+      switch(e.which) {
+        case 32: // Spacebar
+          e.preventDefault();
+          toggle_pause_play();
+          break;
+        case 37: // Left arrow
+        case 38: // Up arrow
+          e.preventDefault();
+          manual_prev_slide();
+          break;
+        case 39: // Right arrow
+        case 40: // Down arrow
+          e.preventDefault();
+          manual_next_slide();
+          break;
+        case 191: // ? (slash key, with shift)
+          if (e.shiftKey) {
+            e.preventDefault();
+            $("#help-overlay").toggleClass("hidden");
+          }
+          break;
+      }
+    });
+
+    // Auto-hide help hint after 5 seconds
+    setTimeout(function() {
+      $("#help-hint").addClass("hidden");
+    }, 5000);
+
     $("#photo-background").height($("#photo-background").height() -
                                   $("#photo-background").position().top);
 
-    refresh_page({'photo': 'slide.php/title',
-                  'title': true,
-                  'next': {'mode': 'slide',
-                           'file': ''}});
-    // We immediately refresh the page again, so any title text placed into
-    // 'next' by the first refresh will now become current.
-    refresh_page({'photo': 'slide.php/title',
-                  'title': true,
-                  'next': {'mode': 'slide',
-                           'file': ''}});
-
-    setInterval(slide_poll, 10000);
+    // Check if we should start at a specific racer (by car number)
+    if (typeof g_start_racerid !== 'undefined' && g_start_racerid > 0) {
+      // Fetch the specific racer
+      var start_query = {
+        'query': 'slide.next',
+        'mode': 'racer',
+        'racerid': g_start_racerid - 1  // Query uses racerid > X
+      };
+      $.ajax('action.php', {
+        type: 'GET',
+        data: start_query,
+        success: function(data) {
+          if (data.hasOwnProperty('photo')) {
+            add_and_display(data.photo);
+          }
+          // Start paused so user can see the specific racer
+          is_paused = true;
+          $("#pause-indicator").addClass("visible");
+        }
+      });
+    } else {
+      // Start with title slide
+      var title_slide = {
+        'photo': 'slide.php/title',
+        'title': true,
+        'next': {'mode': 'slide', 'file': ''}
+      };
+      add_and_display(title_slide);
+      start_slideshow();
+    }
   });
 }());
--- website/slideshow.php
+++ website/slideshow.php
@@ -3,6 +3,19 @@
 <?php
 require_once('inc/banner.inc');
 require_once('inc/photo-config.inc');
+require_once('inc/data.inc');
+
+// Look up racerid from car number if provided
+$start_racerid = 0;
+if (isset($_GET['car'])) {
+  $carnumber = $_GET['car'];
+  $stmt = $db->prepare('SELECT racerid FROM RegistrationInfo WHERE carnumber = :carnumber');
+  $stmt->execute(array(':carnumber' => $carnumber));
+  $row = $stmt->fetch(PDO::FETCH_ASSOC);
+  if ($row) {
+    $start_racerid = intval($row['racerid']);
+  }
+}
 ?><head>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
     <title>DerbyNet Slideshow</title>
@@ -15,6 +28,7 @@ require_once('inc/photo-config.inc');
        var g_kiosk_parameters = <?php
             echo isset($as_kiosk) ? json_encode(kiosk_parameters()) : "{}";
        ?>;
+       var g_start_racerid = <?php echo $start_racerid; ?>;
     </script>
     <script type="text/javascript" src="js/slideshow.js"></script>
   </head>
@@ -25,6 +39,14 @@ require_once('inc/photo-config.inc');
     <div class="next">
       <img class='mainphoto' onload='mainphoto_onload(this)'
            src='slide.php/title'/>
+    </div>
+    <div id="pause-indicator" class="pause_indicator">PAUSED</div>
+    <div id="help-hint" class="help_hint">Press ? for controls</div>
+    <div id="help-overlay" class="help_overlay hidden">
+      <p><b>Spacebar</b> &ndash; Pause / Resume</p>
+      <p><b>&larr; &uarr;</b> &ndash; Previous slide</p>
+      <p><b>&rarr; &darr;</b> &ndash; Next slide</p>
+      <p><b>?</b> &ndash; Toggle this help</p>
     </div>
   </div>
 
